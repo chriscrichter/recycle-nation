f72901296503a74762d3b359431d6c0f291fa82a
Alex Urbini <aurbini92@gmail.com>
2020-02-15 13:18:40 -0500

    built database with associations

---
+++ models/post.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+  const Post = sequelize.define('Post', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: 'personal'
+    }
+  });
+  
+  Post.associate = function(models) {
+    Post.belongsTo(models.User
+      , {
+      foreignKey: {
+        allowNull: false
+      }
+    }
+    );
+  }
+  return Post;
+};
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS recycle_DB;
+-- Creates the "blogger" database --
+CREATE DATABASE recycle_DB;
