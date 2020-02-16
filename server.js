3c01db34cdeb7d822288dff2d58d1a69164569c4
Alex Urbini <aurbini92@gmail.com>
2020-02-16 11:43:03 -0500

    fact-routes

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(express.static("public"));
 // =============================================================
 // require("./routes/html-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
-require("./routes/posts-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
+require("./routes/fact-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
