757ce0499201c88dd0473f8bce377bad0e4592b9
JuJu <58866597+jujubrains@users.noreply.github.com>
2020-02-17 15:43:07 -0500

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,19 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/cms", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  });
+
+  app.get("/posts", function(req,res) {
+    res.sendFile(path.join(__dirname, "..public/post.html"));
+  });
+
+  app.get("/users", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/cms.html"));
   });
 
 };
