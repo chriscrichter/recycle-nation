8573bf383f507a388d7c0055c4c6570c496bf4a8
chriscrichter <chris_richter@outlook.com>
2020-02-16 22:30:45 -0500

    Added handlebars and server.js files.

--- server.js
+++ server.js
@@ -1,38 +1,69 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require("mysql");
+const app = express(),
+      staticServe = express.static(`${ __dirname }/public`);
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-
-// Requiring our models for syncing
-var db = require("./models");
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public/stylesheets'));
+app.use(express.static('public/assets/images'));
 
-// Static directory
-app.use(express.static("public"));
+app.engine('.handlebars', exphbs({
+  defaultLayout: 'main',
+  extname: '.handlebars',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', '.handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Requiring our models for syncing
+//var db = require("./models");
 
 // Routes
 // =============================================================
 // require("./routes/html-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
-require("./routes/fact-api-routes.js")(app);
+//require("./routes/user-api-routes.js")(app);
+//require("./routes/post-api-routes.js")(app);
+//require("./routes/fact-api-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-var PORT = process.env.PORT || 8080;
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+
+var connection = mysql.createConnection({
+  host: "s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "bm8ozsqt8ehomlw5",
+  password: "xr1wsmn48fb916jq",
+  database: "gajb8m09t9ub53b5"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Root get route.
+app.get("/", function(req, res) {
+  connection.query("SELECT fact FROM facts ORDER BY RAND() limit 1;", function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    res.render("index", { facts: data });
   });
 });
 
-//Put { force: true } -- in .sync() to drop and recreate database
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
