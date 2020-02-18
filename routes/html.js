d4a551e4ce4b29b28c83d9939ad727f611d894f5
Alex Urbini <aurbini92@gmail.com>
2020-02-18 16:02:07 -0500

    post handlebar

--- routes/html.js
+++ routes/html.js
@@ -1,53 +1,62 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
-var express = require('express');
+// // Requiring path to so we can use relative routes to our HTML files
+// var path = require("path");
+// var express = require('express');
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+// // Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-var router = express.Router();
+// var router = express.Router();
 
-router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/.html"));
-});
-
-router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-router.get("/signup", function(req, res) {
-  // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/posts");
-  }
-  res.sendFile(path.join(__dirname, "../public/signup.html"));
-});
-
-router.get("/login", function(req, res) {
-  // If the user already has an account send them to the members page
-  if (req.user) {
-    res.redirect("/");
-  }
-  res.sendFile(path.join(__dirname, "../public/login.html"));
-});
-
-// Here we've add our isAuthenticated middleware to this route.
-// If a user who is not logged in tries to access this route they will be redirected to the signup page
-// router.get("/posts", isAuthenticated, function(req, res) {
-//   res.sendFile(path.join(__dirname, "../public/posts.html"));
+// router.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/.html"));
 // });
 
-router.get("/cms", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/cms.html"));
-});
+// router.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
-router.get("/posts", function(req,res) {
-  res.sendFile(path.join(__dirname, "..public/post.html"));
-});
+// router.get("/signup", function(req, res) {
+//   // If the user already has an account send them to the members page
+//   if (req.user) {
+//     res.redirect("/posts");
+//   }
 
-router.get("/users", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/cms.html"));
-});
+//   res.sendFile(path.join(__dirname, "../public/signup.html"));
+// });
+
+// router.get("/login", function(req, res) {
+//   // If the user already has an account send them to the members page
+//   if (req.user) {
+//     res.redirect("/");
+//   }
+//   res.sendFile(path.join(__dirname, "../public/login.html"));
+// });
+
+// // Here we've add our isAuthenticated middleware to this route.
+// // If a user who is not logged in tries to access this route they will be redirected to the signup page
+// // router.get("/posts", isAuthenticated, function(req, res) {
+// //   res.sendFile(path.join(__dirname, "../public/posts.html"));
+// // });
+
+// router.get("/cms", function(req, res) {
+//   console.log('send cms file')
+//   //res.sendFile(path.join(__dirname, "../public/cms.html"));
+//   // res.redirect(__dirname, "../public/cms.html");
+//   //res.sendFile(path.join(__dirname, "../public/blog.html"));
+
+// });
+
+// router.get("/posts", function(req,res) {
+//   //res.sendFile(path.join(__dirname, "../public/blog.html"));
+  
+// });
 
 
-module.exports = router;
+
+// router.get("/users", function(req, res) {
+//   //res.sendFile(path.join(__dirname, "../public/cms.html"));
+
+// });
+
+
+// module.exports = router;
--- routes/twitter.js
+++ routes/twitter.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 router.get('/twitter', function(req, res){
-  res.sendFile(`${process.cwd()}/public/socket.html`);  
+  
+
+ res.sendFile(`${process.cwd()}/public/socket.html`);  
 });
 
 module.exports = router;
