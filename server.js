22ac1ca197152fb0e9ea87180d6ac86362d90b80
Alex Urbini <aurbini92@gmail.com>
2020-02-17 23:21:58 -0500

    multiple rooms twitter

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const mysql = require("mysql");
 
+
+
 var PORT = process.env.PORT || 8080;
+const supportedEnivornments = ['new york', 'chicago', 'los angeles', 'miami', 'seattle', 'portland'];
+// const supportedEnivornments = ['new york'];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -33,31 +37,37 @@ app.use(routes.facts);
 app.use(routes.html);
 app.use(routes.blog);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 io.on('connection', function(socket){
   console.log('a user connected witht he socket id', socket.id);
+  // add connection to the correct room for this user
+  socket.join(supportedEnivornments[getRandomInt(supportedEnivornments.length - 1)])
 });
 
-// send a new tweet emission every 5000 milliseconds
-const interval = setInterval(() => {
-  console.log('emitting new mock tweet');
-  io.emit('new tweet', {
-    text: 'here is a sample tweet',
-    user: 'some user'
+//send a new tweet emission every 5000 milliseconds
+
+
+// const interval = setInterval(() => {
+//   console.log('emitting new mock tweet');
+//   io.emit('new tweet', {
+//     text: 'here is a sample tweet',
+//     user: 'some user'
+//   });
+// }, 5000);
+
+const twitterChannelConfigObject = supportedEnivornments.reduce((channels, env) => {
+  channels[env] = [env];
+  return channels;
+}, {});
+
+const stream = twitter.createStreams(twitterChannelConfigObject);
+
+supportedEnivornments.forEach(supportedEnv => {
+  stream.on('channels/'+supportedEnv, tweet => {
+    io.to(supportedEnv).emit('new tweet', tweet);
   });
-}, 5000);
-
-// twitter.on('data', function(data) {
-//   console.log("got some new twitter data");
-//   io.emit('new tweet', data);
-// })
-
-// twitter.on('error', function(error) {
-//   throw error;
-// });
+})
 
 db.sequelize.sync()
   .then(function() {
--- services/twitter.js
+++ services/twitter.js
@@ -1,9 +1,10 @@
-var Twitter = require('twitter');
-var client = new Twitter({
-  consumer_key: 'wGbATRjTXwtxhfjjs3TTfSSNz',
-  consumer_secret: '1qIh08iAUIf3f0o9lUctZY9xUGCz0zj00RPHPnxFQARF2lTDph',
-  access_token_key: '759962762218962944-CkKCvAWQ9gOOao7b16lY4fyNHVBnSh8',
-  access_token_secret: 'DudaLUjjjDwO7DnYrUrdd4xE6xfI7awCbN33pPdsGhx8S'
-});
-var stream = client.stream('statuses/filter', {track: 'javascript'});
-module.exports = stream;
+const Twitter = require('twitter-stream-channels');
+const twitterConfig = require("../config").twitter;
+const client = new Twitter(twitterConfig);
+module.exports = {
+
+  createStreams: channels => client.streamChannels({ track:channels })
+
+  // stream: () => client.stream('statuses/filter', { track: 'environment' }),
+  // getStream: track => client.stream('statuses/filter', { track })
+};
